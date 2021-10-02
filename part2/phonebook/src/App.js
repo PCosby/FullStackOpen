63ae67943a4449c890d3a4cb25f6e9d82b2b79ec
PCosby <50154976+PCosby@users.noreply.github.com>
2021-10-01 21:11:15 -0400

    Phonebook final

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import {Filter,PersonForm,Persons} from './components/Contact'
 
-const initial = [
-
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-]
 
 
 const App = (props) => {
   
   const [filter, setFilter] = useState("")
-  const [contacts, setContacts] = useState(initial)
+  const [contacts, setContacts] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
-
-
   
 
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons').then(response => {
+        setContacts(response.data)
+      })
+  }, [])
 
   
   return (
